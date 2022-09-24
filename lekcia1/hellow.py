041a7c6cc3de9529130cffba7203550f611df925
Victoriya <victory_alex@mail.ru>
2022-09-24 12:39:13 +0300

    Урок 1. Знакомство с Python Лекции

---
+++ lekcia1/hellow.py
@@ -0,0 +1,59 @@
+#print("hellow world")
+# типы данных и переменна
+# int, float, boolean, str, list, None
+
+# value = None    
+# a = 123
+# b = 1.23
+# value = 12345
+# print (type(a))
+# print (type(b))
+# print (type(value))
+
+# a = 123
+# b = 1.23
+# s = "hellow world"
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'.format(a,b,s))
+# print('{2} - {1} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+# f = False
+# print (f)
+
+#ввод и вывод данных
+# print ('введите имя a')
+# a = int(input())
+# print ('введите имя b')
+# b = int(input())
+# print (a , '+', b, '=' , a+b)
+# print (f"{a}, {b}")
+# print("{}, {}".format(a,b))
+
+# a= 1.3
+# b = 3
+# #c = round(a*b, 3)
+# c = a*b
+# print(c)
+
+# a = int (input('a = '))
+# b= int (input('b = '))
+# if a>b:
+#     print (a)
+# else:
+#     print (b)
+
+перевернутое число
+origin = 32
+inverter = 0
+while origin !=0:
+    inverter = inverter*10+(origin%10)
+    origin //=10
+else:
+    print ('пожалуй')
+    print('хватит')
+print (inverter)
+
+for i in 'qwe - rty':
+    print(i)
+for i in [1,2,3,4,5,5]:
+    print(i)
