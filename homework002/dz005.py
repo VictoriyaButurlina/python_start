1a62fa93dea6a8f7466634ec0799cfbadfefd5e2
Victoriya <victory_alex@mail.ru>
2022-10-02 23:43:08 +0300

    алгоритм перемешивания чисел

--- homework002/dz005.py
+++ homework002/dz005.py
@@ -1,4 +1,9 @@
 # Реализуйте алгоритм перемешивания списка.
 import random
 lst = list(range(1, 10))
+print(type(lst))
 print(lst)
+while lst:  
+    new_list=lst.pop(random.randint(0, len(lst)-1))
+    print(new_list)
+
